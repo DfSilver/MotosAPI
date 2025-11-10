e79c73e69c0c21cc52d32fd87e56d1bbd917d247
SilverD <dfmorenom@academia.usbbog.edu.co>
2025-11-10 06:20:22 +0000

    Fix: added production server configuration

--- app.py
+++ app.py
@@ -21,7 +21,4 @@ def root():
     )
 
 if __name__ == "__main__":
-    from waitress import serve
-    import os
-    port = int(os.environ.get("PORT", 8080))
-    serve(app, host="0.0.0.0", port=port)
+    app.run(debug=True)
